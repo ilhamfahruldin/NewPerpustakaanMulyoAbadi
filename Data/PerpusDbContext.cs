57eaed8bad12d6e2d4b844447b0a4565b022fd45
DwiMlyno <dwimlyno@gmail.com>
2021-11-01 10:34:51 +0700

    menambah admin

--- Data/PerpusDbContext.cs
+++ Data/PerpusDbContext.cs
@@ -1,19 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using Perpustakaan.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace Perpustakaan.Data
 {
-public class PerpusDbContext : IdentityDbContext<Pembaca>
-{
-public PerpusDbContext (DbContextOptions<PerpusDbContext> options) : base(options)
-{ }
-public DbSet <Buku> Bukus { get; set; }
+    public class PerpusDbContext : IdentityDbContext<Pembaca>
+    {
+    public PerpusDbContext (DbContextOptions<PerpusDbContext> options) : base(options)
+    { }
+    public DbSet <Buku> Bukus { get; set; }
 
- public DbSet<TodoList> TodoLists { get; set; }
-public DbSet<TodoItem> TodoItems { get; set; }
+    public DbSet<TodoList> TodoLists { get; set; }
+    public DbSet<TodoItem> TodoItems { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+                .HasData(new [] {
+                    new IdentityRole("Admin"),
+                    new IdentityRole("User")
+                });
 
-}
+            var hasher = new PasswordHasher<Pembaca>();
+            var admin = new Pembaca
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Mulyono",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "dwimul@admin.com",
+                NormalizedEmail = "DWIMUL@ADMIN.COM",
+                EmailConfirmed = true
+            };
+            admin.PasswordHash = hasher.HashPassword(admin, "Abc123$");
+            builder.Entity<Pembaca>()
+                .HasData(admin);
+        }
+
+    }
 }
 
 
--- Migrations/20211101021901_SeedData.Designer.cs
+++ Migrations/20211101021901_SeedData.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Perpustakaan.Data;
 
 namespace Perpustakaan.Migrations
 {
     [DbContext(typeof(PerpusDbContext))]
-    partial class PerpusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211101021901_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
